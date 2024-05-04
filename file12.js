6c99c3819f9bc330aae153f03cae9b599f126cc9
Himanshu <hshekhar014@gmail.com>
2024-05-04 14:46:08 +0530

    First Commit

--- file12.js
+++ file12.js
@@ -19,9 +19,11 @@ console.log(typeof null);
 // bug , error
 
 // BigInt
-let myNumber = 123;
+// let myNumber = 123;
 console.log(myNumber);
 console.log(Number.MAX_SAFE_INTEGER);
 
 let myNumber = 12n;
 console.log(myNumber);
+console.log("Harry");
+
